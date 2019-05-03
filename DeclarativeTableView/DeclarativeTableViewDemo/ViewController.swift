5ad060c14fd04c05999230603d6e08d1f2c83870
vkandel <vijaya.prakash.kandel@zalando.de>
2019-05-03 23:53:56 +0200

    Initial duplicated support for collection view

--- DeclarativeTableView/DeclarativeTableViewDemo/ViewController.swift
+++ DeclarativeTableView/DeclarativeTableViewDemo/ViewController.swift
@@ -17,11 +17,11 @@ class ViewController: UIViewController {
         super.viewWillAppear(animated)
         
         
-        let models = ["Apple", "Microsoft", "Google"]
+        let models = ["Apple", "Microsoft", "Google", "||Zalando is a company that i work currently at.||"]
         let identifier = "MyCell"
 
-        let cellDescs  = models.map { m -> ListCellDescriptor<String, SimpleCell> in
-            var cd = ListCellDescriptor(m, identifier: identifier, cellClass: SimpleCell.self, configure: { cell in
+        let cellDescs  = models.map { m -> CollectionCellDescriptor<String, SimpleCell> in
+            var cd = CollectionCellDescriptor(m, identifier: identifier, cellClass: SimpleCell.self, configure: { cell in
                 cell.textLabel?.text = m
             })
             cd.onSelect = { [weak self] in
@@ -30,34 +30,34 @@ class ViewController: UIViewController {
             return cd
         }
 
-        let sections = ListSectionDescriptor(with: cellDescs)
+        let sections = CollectionSectionDescriptor(with: cellDescs)
         
         
         let modelsForNextSection = [ModelItem(color: .red, int: 1), .init(color: .blue, int: 2), .init(color: .purple, int: 3)]
         let identifier2 = "IntCell"
         let cellDescs2 = modelsForNextSection.map { m in
-            return ListCellDescriptor(m, identifier: identifier2, cellClass: SimpleCell.self, configure: { cell in
+            return CollectionCellDescriptor(m, identifier: identifier2, cellClass: SimpleCell.self, configure: { cell in
                 cell.textLabel?.text = "\(m.int)"
                 cell.backgroundColor = m.color
             })
         }
         
-        let secondSection = ListSectionDescriptor(with: cellDescs2)
+        let secondSection = CollectionSectionDescriptor(with: cellDescs2)
         
         
-        let mc1 = ListCellDescriptor(1, identifier: "mc1", cellClass: SimpleCell.self, configure: { cell in
+        let mc1 = CollectionCellDescriptor(1, identifier: "mc1", cellClass: SimpleCell.self, configure: { cell in
                 cell.textLabel?.text = "\(1)"
             })
         
-        let mc2 = ListCellDescriptor("hello", identifier: "mc2", cellClass: AnotherCell.self, configure: { cell in
+        let mc2 = CollectionCellDescriptor("hello", identifier: "mc2", cellClass: AnotherCell.self, configure: { cell in
                 cell.textLabel?.text = "hello"
                 cell.backgroundColor = .purple
             })
         
-        let mixedSection = ListSectionDescriptor(with: [mc1.any(), mc2.any()])
+        let mixedSection = CollectionSectionDescriptor(with: [mc1.any(), mc2.any()])
         
         let combinedSections = [sections.any(), secondSection.any(), mixedSection]
-        let list = ListViewController(with: combinedSections)
+        let list = CollectionViewController(with: combinedSections)
         embed(list)
     }
     
@@ -69,26 +69,22 @@ class ViewController: UIViewController {
     func tapped(_ item: String) {
         let thisModels = [1,2,3]
         let cellDescs = thisModels.map { item in
-            return ListCellDescriptor(item, identifier: "Inner", cellClass: SimpleCell.self, configure: { cell in
+            return CollectionCellDescriptor(item, identifier: "Inner", cellClass: SimpleCell.self, configure: { cell in
                 cell.textLabel?.text = "\(item)"
             })
         }
-        let sectionDesc = ListSectionDescriptor(with: cellDescs)
-        let list = ListViewController(with: [sectionDesc])
+        let sectionDesc = CollectionSectionDescriptor(with: cellDescs)
+        let list = CollectionViewController(with: [sectionDesc])
         self.show(list, sender: self)
     }
 
     func embed(_ vc: UIViewController) {
         self.currentChild = vc
         self.addChild(vc)
+        vc.view.frame = view.bounds
         self.view.addSubview(vc.view)
-        vc.view.translatesAutoresizingMaskIntoConstraints = false
-        [
-            vc.view.topAnchor.constraint(equalTo: self.view.topAnchor),
-            vc.view.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
-            vc.view.rightAnchor.constraint(equalTo: view.rightAnchor),
-            vc.view.leftAnchor.constraint(equalTo: view.leftAnchor)
-            ].forEach { $0.isActive = true }
+        vc.view.backgroundColor = .white
+        (vc as! UICollectionViewController).collectionView.backgroundColor = .white
         vc.didMove(toParent: self)
     }
 
@@ -100,5 +96,33 @@ class ViewController: UIViewController {
 
 }
 
-class SimpleCell: UITableViewCell { }
-class AnotherCell: UITableViewCell { }
+class SimpleCell: UICollectionViewCell {
+    
+    var textLabel: UILabel!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupLabel()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setupLabel()
+    }
+    
+    func setupLabel() {
+        textLabel = UILabel()
+        self.contentView.addSubview(textLabel)
+        textLabel.translatesAutoresizingMaskIntoConstraints = false
+        [
+            textLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            textLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            textLabel.topAnchor.constraint(equalTo: contentView.topAnchor),
+            textLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+            ].forEach { $0.isActive = true }
+    }
+    
+}
+
+class AnotherCell: SimpleCell {
+}
