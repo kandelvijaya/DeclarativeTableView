009b6e754c8510248c295ba40f55a385d4fe2aaa
vkandel <vijaya.prakash.kandel@zalando.de>
2019-05-04 17:42:38 +0200

    code cleanup

--- DeclarativeTableView/DeclarativeTableView/GenericCollection<T>.swift
+++ DeclarativeTableView/DeclarativeTableView/GenericCollection<T>.swift
@@ -11,10 +11,10 @@ import FastDiff
 import Kekka
 
 
-/// A generic table view controller.
-/// A table view can contain different cells in a section and
+/// A generic collection view controller.
+/// A collection view can contain different cells in a section and
 /// different kinds of sections. This property is acheived by
-/// using a type erased CellDescriptor.
+/// using a type erased CollectionCellDescriptor.
 ///
 /// - note: see `CellDescriptor.any()` for more info
 open class CollectionViewController<T: Hashable>: UICollectionViewController {
@@ -201,60 +201,15 @@ open class CollectionViewController<T: Hashable>: UICollectionViewController {
         return cell
     }
     
-    //    override open func numberOfSections(in tableView: UITableView) -> Int {
-    //        return sectionDescriptors.count
-    //    }
-    
-    //    override open func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    //        return sectionDescriptors[section].items.count
-    //    }
-    
-    //    override open func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    //        let currentItem = model(at: indexPath)
-    //        if let cell = tableView.dequeueReusableCell(withIdentifier: currentItem.reuseIdentifier) {
-    //            currentItem.configure(cell)
-    //            return cell
-    //        } else {
-    //            tableView.register(currentItem.cellClass, forCellReuseIdentifier: currentItem.reuseIdentifier)
-    //            let cell = tableView.dequeueReusableCell(withIdentifier: currentItem.reuseIdentifier, for: indexPath)
-    //            currentItem.configure(cell)
-    //            return cell
-    //        }
-    //    }
-    
     open override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         let currentItem = model(at: indexPath)
         collectionView.deselectItem(at: indexPath, animated: true)
         currentItem.onSelect?()
     }
     
-    //    override open func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-    //        let currentItem = model(at: indexPath)
-    //        tableView.deselectRow(at: indexPath, animated: true)
-    //        currentItem.onSelect?()
-    //    }
-    
-    //    override open func tableView(_ tableView: UITableView, titleForFooterInSection section: Int) -> String? {
-    //        return sectionDescriptors[section].footerText
-    //    }
-    
     override open func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         handlers.onExit?()
-    }
-    
-    override open func viewDidLoad() {
-        super.viewDidLoad()
-        if handlers.onRefreshContents != nil {
-            //            self.refreshControl = UIRefreshControl()
-            //            self.refreshControl?.tintColor = .purple
-            //            self.refreshControl?.addTarget(self, action: #selector(refreshContents), for: UIControl.Event.valueChanged)
-        }
-    }
-    
-    @objc private func refreshContents(_ sender: Any) {
-        self.handlers.onRefreshContents?()
-        //        self.refreshControl?.endRefreshing()
     }
     
 }
